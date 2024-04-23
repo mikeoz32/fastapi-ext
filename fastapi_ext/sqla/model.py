85b412c3cb73567b43548dd79ee5d4a248f8a5d0
Mike Oz <kozloffsky@hotmal.com>
2024-04-23 19:23:21 +0300

    Addedn uuid id mixin

--- fastapi_ext/sqla/model.py
+++ fastapi_ext/sqla/model.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timezone
 from typing import Annotated, TypeVar
-from sqlalchemy import TIMESTAMP, func
+from sqlalchemy import TIMESTAMP, Uuid, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+from uuid import UUID, uuid4
 UTC = timezone.utc
 
 
@@ -19,6 +20,8 @@ class BaseModel:
 class IDMixin(BaseModel):
     id: Mapped[Annotated[int, mapped_column(primary_key=True, autoincrement=True)]]
 
+class UUIDIDMixin(BaseModel):
+    id: Mapped[Annotated[UUID, mapped_column(Uuid, primary_key=True, default=uuid4)]]
 
 class CreatedAtMixin(BaseModel):
     created_at: Mapped[
@@ -64,6 +67,7 @@ M_TS = TypeVar("M_TS", bound=TimestamableMixin)
 __all__ = [
     "Base",
     "IDMixin",
+    "UUIDIDMixin",
     "CreatedAtMixin",
     "UpdatedAtMixin",
     "CreatedUpdatedAtMixin",
